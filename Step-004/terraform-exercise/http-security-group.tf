91ff12d239a6dcd749b0cb4f5cc3931ffe41a8cb
Michał Wójtowicz <michal.wojtowicz@siemens.com>
2020-04-14 10:54:00 +0200

    Split security groups into two files

--- Step-004/terraform-exercise/http-security-group.tf
+++ Step-004/terraform-exercise/http-security-group.tf
@@ -1,6 +1,5 @@
 locals {
   security_group_name = "http-inbound-traffic"
-  ssh_security_group_name = "ssh-inbound-traffic"
 }
 
 resource "aws_security_group" "http-inbound-traffic" {
@@ -33,32 +32,6 @@ resource "aws_security_group" "http-inbound-traffic" {
 
   tags = {
     Name = local.security_group_name
-    Terraform = "true"
-  }
-}
-
-resource "aws_security_group" "ssh-inbound-traffic" {
-  name        = local.ssh_security_group_name
-  description = "Allow SSH inbound traffic"
-  vpc_id      = aws_vpc.indacloud-stepbystep.id
-
-  ingress {
-    description = "SSH inbound rule"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = local.ssh_security_group_name
     Terraform = "true"
   }
 }
